ffc0c67ed99ddc81f133ad50a2ea23b3504fcce9
browenchen <owenchen@berkeley.edu>
2016-03-07 22:46:43 -0800

    first version

---
+++ app/controllers/activities_controller.rb
@@ -0,0 +1,128 @@
+class ActivitiesController < ApplicationController
+	before_action :set_activity, only: [:show, :edit, :update, :destroy]  
+
+	def index
+	  @activities = Activity.all
+	end
+
+	def new
+	  @activity = Activity.new
+	end
+	
+	def create
+	  if @activity = Activity.create(activity_params)
+	  	flash[:success] = "Your activity has been created!"
+	  	redirect_to activities_path
+	  else 
+      	flash.now[:alert] = "Your new activity couldn't be created!  Please check the form."
+      	render :new	 
+      end 	
+	end
+
+	def show
+	  @activity = Activity.find(params[:id])
+	end
+
+	def edit  
+	end
+
+	def update  
+  	  if @activity.update(activity_params)
+      	flash[:success] = "Activity updated."
+      	redirect_to activities_path  
+      else
+        flash.now[:alert] = "Update failed.  Please check the form."
+        render :edit
+      end      		  	
+  	  # redirect_to(activity_path(@activity))
+	end  
+
+	def destroy
+	  @activity = Activity.find(params[:id])
+	  @activity.destroy
+	  redirect_to activities_path	  
+	end
+
+	private 
+
+	def activity_params
+	  params.require(:activity).permit(:image, :content)
+	end
+
+    def set_activity
+      @activity = Activity.find(params[:id])
+    end	
+
+  # before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  # # GET /posts
+  # # GET /posts.json
+  # def index
+  #   @posts = Post.all
+  # end
+
+  # # GET /posts/1
+  # # GET /posts/1.json
+  # def show
+  # end
+
+  # # GET /posts/new
+  # def new
+  #   @post = Post.new
+  # end
+
+  # # GET /posts/1/edit
+  # def edit
+  # end
+
+  # # POST /posts
+  # # POST /posts.json
+  # def create
+  #   @post = Post.new(post_params)
+
+  #   respond_to do |format|
+  #     if @post.save
+  #       format.html { redirect_to @post, notice: 'Post was successfully created.' }
+  #       format.json { render :show, status: :created, location: @post }
+  #     else
+  #       format.html { render :new }
+  #       format.json { render json: @post.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+
+  # # PATCH/PUT /posts/1
+  # # PATCH/PUT /posts/1.json
+  # def update
+  #   respond_to do |format|
+  #     if @post.update(post_params)
+  #       format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @post }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @post.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+
+  # # DELETE /posts/1
+  # # DELETE /posts/1.json
+  # def destroy
+  #   @post.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
+
+  # private
+  #   # Use callbacks to share common setup or constraints between actions.
+  #   def set_post
+  #     @post = Post.find(params[:id])
+  #   end
+
+  #   # Never trust parameters from the scary internet, only allow the white list through.
+  #   def post_params
+  #     params.fetch(:post, {})
+  #   end	
+end
---
+++ app/models/activity.rb
@@ -0,0 +1,9 @@
+class Activity < ActiveRecord::Base
+  validates :image, presence: true
+
+  has_attached_file :image
+  # validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+
+end

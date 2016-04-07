2c490f48776373d676267d05f79adfd43319a28a
browenchen <owenchen@berkeley.edu>
2016-04-07 13:39:42 -0700

    changed activity container into a partial that contains all buttons, the container for experiement, and update activity function. This entire view changes now when we select a different activity

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160406063819) do
+ActiveRecord::Schema.define(version: 20160407181334) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "title"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 20160406063819) do
     t.text     "experimental_condition", default: "Initial condition"
     t.decimal  "points"
     t.string   "experiment_state"
+    t.boolean  "is_active",              default: true
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

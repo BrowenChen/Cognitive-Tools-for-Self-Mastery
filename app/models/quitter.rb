2c5e1f90dffad5b937eef80ddfed67940ebb80ad
Eric Zhang <ezhang@Erics-MacBook-Pro.local>
2016-04-17 17:49:07 -0700

    finished priority 3

---
+++ app/models/quitter.rb
@@ -0,0 +1,2 @@
+class Quitter < ActiveRecord::Base
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411210353) do
+ActiveRecord::Schema.define(version: 20160418004810) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "title"
@@ -45,6 +45,13 @@ ActiveRecord::Schema.define(version: 20160411210353) do
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
+
+  create_table "quitters", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.string   "time_quit"
+  end
 
   create_table "todos", force: :cascade do |t|
     t.datetime "created_at", null: false

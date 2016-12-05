b058dad2616b2d8018712cac73ca2fa1132c7c2c
Owen Chen <owchen@linkedin.com>
2016-12-04 16:38:43 -0800

    remove email validation from devise authentication and user model. Signups and login's are username only based

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,28 @@ class User < ActiveRecord::Base
   has_many :activities, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+  attr_accessor :login
+
   validates :user_name, presence: true, length: { minimum: 4, maximum: 16 }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+   conditions = warden_conditions.dup
+   if login = conditions.delete(:login)
+     where(conditions).where(["user_name = :value", { :value => login }]).first
+   else
+     where(conditions).first
+   end
+  end
+
+  #dont require email
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160817050529) do
+ActiveRecord::Schema.define(version: 20161205003610) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "title"
@@ -81,7 +81,6 @@ ActiveRecord::Schema.define(version: 20160817050529) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                   default: "",                  null: false
     t.string   "encrypted_password",      default: "",                  null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -103,7 +102,6 @@ ActiveRecord::Schema.define(version: 20160817050529) do
     t.boolean  "finished_all_activities", default: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   add_index "users", ["user_name"], name: "index_users_on_user_name", unique: true
 

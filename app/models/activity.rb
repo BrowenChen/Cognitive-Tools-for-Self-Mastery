bf6006036fb6341d375115a73b79d6d929cc7c5b
Jim Ruther Nill <jvnill@gmail.com>
2017-04-22 01:26:11 +1000

    remove association between point and activity

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,5 @@
 class Activity < ActiveRecord::Base
   has_many :comments, dependent: :destroy
-  has_many :points, dependent: :destroy
   has_many :answers, dependent: :destroy, inverse_of: :activity
 
   belongs_to :user, inverse_of: :activities
